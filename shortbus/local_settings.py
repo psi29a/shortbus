842bfacef56c45a21ba4aad68a80d5ff568eda58
Bret Curtis <psi29a@gmail.com>
2014-12-01 13:56:00 +0100

    initial push of shortbus, pure text, no data

---
+++ shortbus/local_settings.py
@@ -0,0 +1,8 @@
+# all tweakables that can be found in djangobb_forum/settings.py
+# which are all prepended with DJANGOBB_
+
+DJANGOBB_FORUM_BASE_TITLE = 'Project Shortbus'
+DJANGOBB_HEADER = 'Shortbus'
+DJANGOBB_TAGLINE = 'Because it is retarded...'
+DJANGOBB_NOTICE = 'Under heavy development!'
+
---
+++ shortbus/settings.py
@@ -0,0 +1,222 @@
+"""
+Django settings for shortbus project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = '399ha3=@sot%=k+dw8=6cpjcsjtwidk$#v+hz#&s5c$r_8+c7@'
+
+# Make this unique, and don't share it with anybody.
+
+if not hasattr(globals(), 'SECRET_KEY'):
+    SECRET_FILE = os.path.join(BASE_DIR, 'secret')
+    try:
+        SECRET_KEY = open(SECRET_FILE).read().strip()
+    except IOError:
+        try:
+            from random import choice
+            import string
+            symbols = ''.join((string.lowercase, string.digits, string.punctuation ))
+            SECRET_KEY = ''.join([choice(symbols) for i in range(50)])
+            secret = file(SECRET_FILE, 'w')
+            secret.write(SECRET_KEY)
+            secret.close()
+        except IOError:
+            raise Exception('Please create a %s file with random characters to generate your secret key!' % SECRET_FILE)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+SITE_ID = 1
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+FORCE_SCRIPT_NAME = ''
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'django_authopenid.context_processors.authopenid',
+    'django_messages.context_processors.inbox',
+    'djangobb_forum.context_processors.forum_settings',
+)
+
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sitemaps',
+    'django.contrib.staticfiles',
+    'django_messages',
+    'djangobb_forum',
+    'haystack',
+    'registration',
+    'django_authopenid',
+    'linaro_django_pagination',
+    'shortbus.home',
+    'shortbus.field',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+    'django_authopenid.middleware.OpenIDMiddleware',
+    'djangobb_forum.middleware.LastLoginMiddleware',
+    'djangobb_forum.middleware.UsersOnline',
+    'djangobb_forum.middleware.TimezoneMiddleware',
+    'linaro_django_pagination.middleware.PaginationMiddleware',
+)
+
+ROOT_URLCONF = 'shortbus.urls'
+
+WSGI_APPLICATION = 'shortbus.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ATOMIC_REQUESTS': True
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LANGUAGES = (
+    ('ca', 'Catalan'),
+    ('cs', 'Czech'),
+    ('de', 'German'),
+    ('en', 'English'),
+    ('es', 'Spanish'),
+    ('fo', 'Faroese'),
+    ('fr', 'France'),
+    ('it', 'Italian'),
+    ('lt', 'Lithuanian'),
+    ('mn', 'Mongolian'),
+    ('nl', 'Dutch'),
+    ('pl', 'Polish'),
+    ('ru', 'Russian'),
+    ('uk_UA', 'Ukrainian'),
+    ('vi', 'Vietnamese'),
+    ('zh_CN', 'Chinese'),
+)
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT,'openid/static'),
+    os.path.join(PROJECT_ROOT,'djangobb_forum/static'),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Haystack settings
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(BASE_DIR, 'djangobb_index'),
+        'INCLUDE_SPELLING': True,
+    },
+}
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+# Account settings
+ACCOUNT_ACTIVATION_DAYS = 10
+LOGIN_REDIRECT_URL = '/forum/'
+LOGIN_URL = '/account/signin/'
+
+try:
+    import mailer
+    INSTALLED_APPS += ('mailer',)
+    EMAIL_BACKEND = "mailer.backend.DbBackend"
+except ImportError:
+    pass
+
+#Cache settings
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
